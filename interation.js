654829f15184a1e944411c58e5b07fa482653abb
Rafael William <rafaelwilliansgi4@gmail.com>
2021-05-26 23:35:20 -0300

    commitado_apenas

--- interation.js
+++
@@ -1,1 +0,0 @@
-//window.alert('Página em desenvolvimento!');
--- request.php
+++ request.php
@@ -3,38 +3,48 @@
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <link rel="stylesheet" href="style.css">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link rel="stylesheet" href="estilo_cadastro.css">
+    <style>
+        h3, h4 {
+            color:white;
+            text-align:center;
+        }
+    </style>
+    <title>Apostas Online</title>
 </head>
 <body>
-<div id="all">
-<div class="white">
-    <a href="#">logo</a>
-</div>
-<h2>Atendimento em casa</h2>
-<hr>
-<header>
-    
-</header>
-<section>
-    <?php
-        $nome = $_POST['nome_user'];
-        echo "<h3>Olá ".$nome."<h3>";
-    ?>
-    <div class="resposta">
-        <h2>Cadastro realizado com sucesso!</h2>
-        <a href="consulta.php"><input type="submit" class="pxt" name="" id="" value="Acompanhar minha consulta"></a>
-        <a href="index.html"><input type="submit" class="pxt" name="" id="" value="Voltar ao cadastro"></a>
-    </div>
-    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Facere reiciendis omnis recusandae magnam quibusdam aspernatur autem cumque, quidem fugiat, aut ratione. Aperiam accusantium corrupti praesentium porro perferendis fugit ullam dolor!</p>
-</section>
-<footer>
-    <div class="white1">
-    Imagem rodapé
-    </div>
-    <p>Todos os direitos reservados - 2021 -<a href="https://www.instagram.com/willtry.tar/" target="_blank"> @Rafael William</a></p>
-</footer>
-</div>
+    <section>
+        <nav class="menu">
+            <ul>
+                <li>Home</li>
+                <li>Cadastro</li>
+                <li>Notícias</li>
+                <li>Login</li>
+            </ul>
+            <br>
+        </nav>
+        <form action="request.php" method="POST">
+
+            <fieldset> <legend><strong></strong></legend>
+                <br>
+                    <?php
+
+                        $nome = $_POST['nome'];
+                        $email = $_POST['email'];
+                        $usuario = $_POST['usuario'];
+
+                        echo "<h3>Olá $usuario</h3><br>";
+                        echo "<h4>Seja bem vindo a nossa plataforma!</h4>"
+
+                    ?>
+                    <a href="cadastro.html"><input type="button" name="submit" id="submit" value="Continuar"></a>
+                <br><br>
+            </fieldset>
+        </form>
+    </section>
+    <footer>
+
+    </footer>
 </body>
 </html>
